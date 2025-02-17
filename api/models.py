32dc34b41c30e4a04523298e7a8900dab5a95e2e
Matu-sunuwawa <matyassinaadugna@gmail.com>
2025-02-17 07:06:59 -0500

    Backend Fully Functional JWT

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
-from django.db import models
 
-# Create your models here.
+# when Apply `Access Token Blacklisting` needed:
+
+# from django.db import models
+# from django.contrib.auth.models import User
+
+# class BlacklistedToken(models.Model):
+#     token = models.CharField(max_length=500, unique=True)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+#     blacklisted_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.token
--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
-
 from django.urls import path
-from .views import Home
+from .views import Home, Logout
 
 
 urlpatterns = [
     path('', Home.as_view()),
+    path('logout/', Logout.as_view(), name='logout'),
 ]
