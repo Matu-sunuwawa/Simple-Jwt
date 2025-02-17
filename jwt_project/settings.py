cf8cbcb197ca3b72ae1a2d5c77618b0c20adb2b5
Matu-sunuwawa <matyassinaadugna@gmail.com>
2025-02-17 07:23:40 -0500

    update settings.py and ReadMe for Backend JWT.

--- jwt_project/settings.py
+++ jwt_project/settings.py
@@ -56,7 +56,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=3),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,  # Rotate refresh tokens to prevent reuse
     'BLACKLIST_AFTER_ROTATION': True,  # Blacklist old refresh tokens after rotation
